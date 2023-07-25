a540d6b7d46c9d7eedc2f991d8e47c12ec0261c9
Sanorian <krajnov.sascha@yandex.ru>
2023-07-25 10:52:42 +0400

    Merge 2 buttons to 1

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-let note, localNote = localStorage.getItem('note'), textarea = document.getElementById('notes'), light_mode_button = document.getElementById('light_mode'), dark_mode_button = document.getElementById('dark_mode');
-/// Colors
+let note, localNote = localStorage.getItem('note'), textarea = document.getElementById('notes'), change_mode_button = document.getElementById('change_mode');
+
 textarea.textContent = (localNote ?? 'Попробуйте что-нибудь написать');
-light_mode_button.onclick = function(){
-  document.getElementById('main').classList.remove('main_light');
-  document.getElementById('main').classList.add('main_dark');
-  document.getElementById('body').classList.remove('body_light');
-  document.getElementById('body').classList.add('body_dark');
-  textarea.classList.remove('textarea_light');
-  textarea.classList.add('textarea_dark');
-}
-dark_mode_button.onclick = function(){
-  document.getElementById('main').classList.remove('main_dark');
-  document.getElementById('main').classList.add('main_light');
-  document.getElementById('body').classList.remove('body_dark');
-  document.getElementById('body').classList.add('body_light');
-  textarea.classList.remove('textarea_dark');
-  textarea.classList.add('textarea_light');
-}
+
 textarea.oninput = function() {
   note = textarea.value;
   localStorage.setItem('note', note);
+}
+change_mode_button.onclick = function(){
+  if (textarea.getAttribute('class')=='textarea_light')  {
+    document.getElementById('main').classList.remove('main_light');
+    document.getElementById('main').classList.add('main_dark');
+    document.getElementById('body').classList.remove('body_light');
+    document.getElementById('body').classList.add('body_dark');
+    textarea.classList.remove('textarea_light');
+    textarea.classList.add('textarea_dark');
+  } else {
+    document.getElementById('main').classList.remove('main_dark');
+    document.getElementById('main').classList.add('main_light');
+    document.getElementById('body').classList.remove('body_dark');
+    document.getElementById('body').classList.add('body_light');
+    textarea.classList.remove('textarea_dark');
+    textarea.classList.add('textarea_light');
+  }
 }
