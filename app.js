52c10f21ad00018e65eefdfd1177931e449ed6cf
Sanorian <krajnov.sascha@yandex.ru>
2024-01-06 21:34:38 +0400

    Fix bugs

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
-let localNote = localStorage.getItem('note'), 
-  textarea = document.getElementsByTagName("textarea")[0],
-  mode=(localStorage.getItem('mode')??'light');
+document.addEventListener("DOMContentLoaded", (event) => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const linkedNote = decodeURI(urlParams.get('note'));
+  if (linkedNote!=null && linkedNote!="" && linkedNote!="null") {
+    localStorage.setItem("oldNote", localStorage.getItem('note'));
+    document.getElementsByTagName("textarea")[0].textContent = (linkedNote ?? localStorage.getItem('note') ?? '');
+  }
+  else {
+    document.getElementsByTagName("textarea")[0].textContent = (localStorage.getItem("oldNote") ?? localStorage.getItem('note') ?? '');
+    localStorage.removeItem("oldNote");
+  }
 
-const urlParams = new URLSearchParams(window.location.search);
-const linkedNote = decodeURI(urlParams.get('note'));
-if (linkedNote!=null && linkedNote!="" && linkedNote!="null") {
-  localStorage.setItem("oldNote", localNote);
-  textarea.textContent = (linkedNote ?? localNote ?? '');
-}
-else {
-  textarea.textContent = (localStorage.getItem("oldNote") ?? localNote ?? '');
-  localStorage.removeItem("oldNote");
+  if ((localStorage.getItem('mode')??'light')=='dark')  {
+    setDarkTheme();
 }
 
-if (mode=='dark')  {
-  setDarkTheme();
-}
+});
 
 function saveNotes() {
-  note = textarea.value;
+  note = document.getElementsByTagName("textarea")[0].value;
   localStorage.setItem('note', note);
 }
 
 function changeTheme(){
-  if (textarea.getAttribute('class')=='textarea_light')  {
+  if (document.getElementsByTagName("textarea")[0].getAttribute('class')=='textarea_light')  {
     setDarkTheme();
   } else {
     setLightTheme();
   }
 }
 function clearNote(){
-  textarea.value = '';
+  document.getElementsByTagName("textarea")[0].value = '';
   localStorage.setItem('note', '');
 }
 function downloadNote(){
   var date = new Date();
   const link = document.createElement("a");
-  const file = new Blob([textarea.value], { type: 'text/plain' });
+  const file = new Blob([document.getElementsByTagName("textarea")[0].value], { type: 'text/plain' });
   link.href = URL.createObjectURL(file);
   link.download = "note(" + date.getDate() +"."+ date.getMonth() + "." + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ").txt";
   link.click();
@@ -48,8 +47,8 @@ function setDarkTheme(){
   document.getElementsByTagName('main')[0].classList.add('main_dark');
   document.getElementsByTagName('body')[0].classList.remove('body_light');
   document.getElementsByTagName('body')[0].classList.add('body_dark');
-  textarea.classList.remove('textarea_light');
-  textarea.classList.add('textarea_dark');
+  document.getElementsByTagName("textarea")[0].classList.remove('textarea_light');
+  document.getElementsByTagName("textarea")[0].classList.add('textarea_dark');
   Array.from(document.getElementsByTagName("button")).forEach((button)=>{
     button.classList.remove('button_light');
     button.classList.add('button_dark');
@@ -62,8 +61,8 @@ function setLightTheme(){
   document.getElementsByTagName('main')[0].classList.add('main_light');
   document.getElementsByTagName('body')[0].classList.remove('body_dark');
   document.getElementsByTagName('body')[0].classList.add('body_light');
-  textarea.classList.remove('textarea_dark');
-  textarea.classList.add('textarea_light');
+  document.getElementsByTagName("textarea")[0].classList.remove('textarea_dark');
+  document.getElementsByTagName("textarea")[0].classList.add('textarea_light');
   Array.from(document.getElementsByTagName("button")).forEach((button)=>{
     button.classList.remove('button_dark');
     button.classList.add('button_light');
@@ -75,14 +74,14 @@ function shareNote(){
   if (document.getElementById("shareDiv").getAttribute('class')=="sharePlaceRolled"){
     const link = encodeURI(window.location.href+"?note="+document.getElementsByTagName("textarea")[0].value);
     document.getElementById("shareDiv").classList.remove("sharePlaceRolled");
-    if (textarea.getAttribute('class')=='textarea_light')  {
+    if (document.getElementsByTagName("textarea")[0].getAttribute('class')=='textarea_light')  {
       document.getElementById("shareDiv").classList.add("sharePlaceExpandedLight");
     } else {
       document.getElementById("shareDiv").classList.add("sharePlaceExpandedDark");
     }
     document.getElementsByTagName("code")[0].innerHTML += link;
   } else {
-    if (textarea.getAttribute('class')=='textarea_light')  {
+    if (document.getElementsByTagName("textarea")[0].getAttribute('class')=='textarea_light')  {
       document.getElementsByTagName("code")[0].innerHTML = '<button class="button_light" onclick="copyLink()">Copy</button>';
       document.getElementById("shareDiv").classList.remove("sharePlaceExpandedLight");
     } else {
